9a867f03c1fdc27a9887483309067d68396752a6
EungChang-Mason-Lee <eungchang_mason@kaist.ac.kr>
2021-04-07 05:28:13 +0900

    Update cv_images_to_mp4.py

--- cv_images_to_mp4.py
+++ cv_images_to_mp4.py
@@ -1,17 +1,19 @@
 import cv2
 import os
 
-folder = '/home/cps/images/'
+folder = '/home/cps/images/19/'
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))
+out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (640,480))
 
-for filename in os.listdir(folder):
+files = os.listdir(folder)
+files.sort()
+for filename in files:
     img = cv2.imread(os.path.join(folder,filename))
     frame = cv2.resize(img, dsize=(640, 480), interpolation=cv2.INTER_AREA) #image should be in size same with out video
 
     out.write(frame)
     cv2.imshow('frame',frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release everything if job is finished
