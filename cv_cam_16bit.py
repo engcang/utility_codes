bed62d5e132d9a04b6c2079ee8de8efe99b536e3
EungChang-Mason-Lee <eungchang_mason@kaist.ac.kr>
2021-04-06 15:44:33 +0900

    Update cv_cam_16bit.py

--- cv_cam_16bit.py
+++ cv_cam_16bit.py
@@ -2,6 +2,15 @@ import cv2
 import numpy as np
 import time
 
+def img_to_8bit_img(bit_img):
+    minVal = np.amin(bit_img); maxVal = np.amax(bit_img)
+    it_img = bit_img - minVal
+    it_img = it_img / float(maxVal-minVal) * 255.0 #### bing shin
+    return it_img.astype('uint8') 
+#   it_img = it_img / float(maxVal-minVal) #### bing shin
+#   return it_img
+
+
 cam = cv2.VideoCapture('/dev/video0')
 print(cam.get(cv2.CAP_PROP_FORMAT))
 print(cam.get(cv2.CAP_PROP_MODE))
@@ -27,21 +36,22 @@ while True:
         #channel1=frame[:,:,0]
         #channel2=frame[:,:,1]
         #channel3=frame[:,:,2]
-        #horizon_tmp = cv2.hconcat([channel1, channel2])
-        #horizon = cv2.hconcat([horizon_tmp, channel3])
-        minVal = np.amin(frame)
-        maxVal = np.amax(frame)
-        #print(frame[120:122,:])
-        print(frame)
+        #horizon_tmp = cv2.hconcat([channel1, channel2]); horizon = cv2.hconcat([horizon_tmp, channel3])
+        minVal = np.amin(frame); maxVal = np.amax(frame)
+        
         img = frame[:120,:]
-        #print(maxVal)
-#        draw = cv2.convertScaleAbs(frame, alpha=255.0/(maxVal - minVal), beta=0)
-        #draw = cv2.convertScaleAbs(frame, alpha=1, beta=0)
-        draw = cv2.convertScaleAbs(img) # 8-bit to 16-bit converting
-        #cv2.imshow('Thermal image URL', horizon)
-        cv2.imshow('Thermal image URL', draw)
 
-        #cv2.moveWindow('Thermal image URL',0,0)
+        thermal_calib = 0.0217*(frame_120160-8192.0)+293.0-273.0
+        gray_8bit = img_to_8bit_img(img)
+        colored_img = cv2.applyColorMap(gray_8bit, cv2.COLORMAP_JET)
+        
+        #draw = cv2.convertScaleAbs(frame, alpha=255.0/(maxVal - minVal), beta=0)
+        #draw = cv2.convertScaleAbs(frame, alpha=1, beta=0)
+        #draw = cv2.convertScaleAbs(img) # 16-bit to 8-bit converting
+        #cv2.imshow('Thermal image URL', horizon)
+        #cv2.imshow('Thermal image URL', draw)
+        cv2.imshow('Thermal image URL', colored_img)
+
         if cv2.waitKey(1)==ord('q'):
             break
 cam.release()
