6958a16723edc348d3c0cdfae6e655ce487faf5f
EungChang-Mason-Lee <eungchang_mason@kaist.ac.kr>
2021-05-13 03:41:40 +0900

    Create bag2img.py

---
+++ bag2img.py
@@ -0,0 +1,38 @@
+import time, sys, os
+from ros import rosbag
+import roslib, rospy
+roslib.load_manifest('sensor_msgs')
+from sensor_msgs.msg import Image
+
+from cv_bridge import CvBridge
+import cv2
+
+import sys
+import signal
+def signal_handler(signal, frame): # ctrl + c -> exit program
+        print('You pressed Ctrl+C!')
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+def Bagtoimage(bagname, folder='./'):
+    bridge=CvBridge()
+    topics = []
+    for topic, msg, t in rosbag.Bag(bagname):
+        if topic not in topics:
+            topics.append(topic)
+
+    for topi in topics:
+        img_name_prefix="_".join(topic.split('/'))
+        for topic, msg, t in rosbag.Bag(bagname):
+            if topic == topi:
+                img=bridge.imgmsg_to_cv2(msg, "bgr8")
+                cv2.imshow('test', img)
+                cv2.waitKey(1)
+                print(folder+'/'+img_name_prefix+'_%06d.png'%(msg.header.seq))
+                cv2.imwrite(folder+'/'+img_name_prefix+'_%06d.png'%(msg.header.seq), img)
+
+if __name__ == "__main__":
+    if len( sys.argv ) == 3:
+        Bagtoimage(*sys.argv[1:])
+    else:
+        print( "Usage: python bag2img.py bagfilename output_foldername")
