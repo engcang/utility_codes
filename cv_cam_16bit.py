720b2128cdb887e33ba1bbd9172bf033c34087c6
EungChang-Mason-Lee <eungchang_mason@kaist.ac.kr>
2021-07-19 18:35:47 +0900

    Update cv_cam_16bit.py

--- cv_cam_16bit.py
+++ cv_cam_16bit.py
@@ -12,56 +12,44 @@ def img_to_8bit_img(bit_img):
 
 
 
-#cam2 = cv2.VideoCapture('/dev/video1') 
 cam = cv2.VideoCapture('/dev/video5')
-#cam = cam2
 
-
-
-#backtorgb = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
-
-
-
-print(cam.get(cv2.CAP_PROP_FORMAT))
+#print(cam.get(cv2.CAP_PROP_FORMAT))
 #print(cam.get(cv2.CAP_PROP_MODE))
 
 cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('Y','1','6',' '))
-cam.set(cv2.CAP_PROP_CONVERT_RGB, False)
+#cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('Y','1','6',' '))
 
+cam.set(cv2.CAP_PROP_CONVERT_RGB, False)
 cam.set(cv2.CAP_PROP_FORMAT,cv2.CV_16U)
 cam.set(cv2.CAP_PROP_CONVERT_RGB, False)
 cam.set(cv2.CAP_PROP_MODE, 3)  
 #print(cam.get(cv2.CAP_PROP_FORMAT))
 #print(cam.get(cv2.CAP_PROP_MODE))
 
-
-
-
-
 time.sleep(1)
 count = 0
+width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 while True:
     _, frame = cam.read()
-#    _, frame2 = cam2.read()
-
-#    print(np.shape(frame))
-
-    frame_120160 = frame[:120,:]
-
     count=count+1
+    if count>10:
+    #    print(np.shape(frame))
+        frame_120160 = frame[:120,:]
 
-    thermal_frame = frame[:120,:]
-    color_temperature = frame[:120,:]
+        thermal_calib = 0.0217*(frame_120160-8192.0)+293.0-273.0
+        gray_8bit = img_to_8bit_img(frame_120160)
+        colored_img = cv2.applyColorMap(gray_8bit, cv2.COLORMAP_JET)
+        colored_img2 = cv2.resize(colored_img,(int(3*width),int(3*height)),interpolation = cv2.INTER_CUBIC)
 
-    thermal_calib = 0.0217*(thermal_frame-8192.0)+293.0-273.0
-
-    gray_8bit = img_to_8bit_img(frame_120160)
-    colored_img = cv2.applyColorMap(gray_8bit, cv2.COLORMAP_JET)
-
-    cv2.imshow('Thermal', colored_img)
-
-    if cv2.waitKey(1)==ord('q'):
-        break
+        #hot_y = np.argmax(thermal_calib)//160
+        #hot_x = np.argmax(thermal_calib)%160
+        #hot_temp = np.amax(thermal_calib)
+        
+        cv2.imshow('Thermal', colored_img2)
+        if cv2.waitKey(1)==ord('q'):
+            break
 cam.release()
 cv2.destroyAllWindows()
