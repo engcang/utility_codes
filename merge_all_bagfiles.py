bb2537467a6d9bd496c650df937b40ef1aa9f2ca
Eungchang-Mason-Lee <eungchang_mason@kaist.ac.kr>
2024-02-09 20:58:44 +0900

    Add files via upload

---
+++ merge_all_bagfiles.py
@@ -0,0 +1,26 @@
+import os
+import rosbag
+
+def merge_bag_files(input_directory, output_bag_file):
+    """
+    Merges all .bag files in the specified directory into a single new bag file.
+    
+    :param input_directory: The path to the directory containing the .bag files to merge.
+    :param output_bag_file: The path and name of the output merged bag file.
+    """
+    # List all bag files in the input directory
+    bag_files = [os.path.join(input_directory, f) for f in os.listdir(input_directory) if f.endswith('.bag')]
+    bag_files.sort()  # Optional: sort files by name
+
+    with rosbag.Bag(output_bag_file, 'w') as outbag:
+        for bag_file in bag_files:
+            print(f"Merging {bag_file}...")
+            with rosbag.Bag(bag_file, 'r') as inbag:
+                for topic, msg, t in inbag.read_messages():
+                    outbag.write(topic, msg, t)
+    print(f"All bag files have been merged into {output_bag_file}")
+
+# Example usage
+input_directory = '/home/mason/Downloads/test2'
+output_bag_file = '/home/mason/Downloads/test2_merged.bag'
+merge_bag_files(input_directory, output_bag_file)
